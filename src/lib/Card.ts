e3be46c0b330b6c5e6a3b380bdca6669a47a6d2d
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-15 18:39:51 +0600

    Level 2 done

---
+++ src/lib/Card.ts
@@ -0,0 +1,8 @@
+class Card {
+    constructor(public rank: string, public suit: string) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
+
+export default Card;
---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,41 @@
+import Card from './Card.ts';
+
+class CardDeck {
+    public cards: Card[];
+
+    constructor() {
+        this.cards = [];
+
+        const ranks: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const suits: string[] = ['diams', 'hearts', 'clubs', 'spades'];
+
+        ranks.map(rank => {
+            suits.map(suit => {
+                return this.cards.push(new Card(rank, suit));
+            });
+        });
+    }
+
+    getCard(): Card | null {
+        if (this.cards.length === 0) return null;
+
+        const randomCard: number = Math.floor(Math.random() * this.cards.length);
+
+        return this.cards.splice(randomCard, 1)[0];
+    }
+
+    getCards(amount: number): Card[] {
+        const cardsAmount: Card[] = [];
+
+        for (let i = 0; i < amount; i++) {
+            const card: Card | null = this.getCard();
+
+            if (card) {
+                cardsAmount.push(card);
+            }
+        }
+        return cardsAmount;
+    }
+}
+
+export default CardDeck;
