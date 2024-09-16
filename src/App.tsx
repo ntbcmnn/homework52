5d365395dccb810bd7b6a48ac7d31ca760c0265e
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-16 14:15:11 +0600

    Added a function to draw a new deck if the prev. deck is empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
+import {useState} from 'react';
 import './App.css';
 import PlayingCard from './components/Card/PlayingCard.tsx';
 import Card from './lib/Card.ts';
 import CardDeck from './lib/CardDeck.ts';
-import {useState} from 'react';
+import PokerHand from './lib/PokerHand.ts';
 
 const App = () => {
     const [cards, setCards] = useState<{ rank: string, suit: string }[]>([]);
+    const [deck, setDeck] = useState<CardDeck>(new CardDeck());
+    const hand: PokerHand = new PokerHand(cards);
 
     const dealCards = (): void => {
-        const deck: CardDeck = new CardDeck();
+        if (deck.cards.length === 0) {
+            dealNewDeck();
+        } else {
+            const drawCards: Card[] = deck.getCards(5);
+            setCards(drawCards);
+        }
+    };
+
+    const dealNewDeck = (): void => {
+        const newDeck: CardDeck = new CardDeck();
+        setDeck(newDeck);
+
         const drawCards: Card[] = deck.getCards(5);
         setCards(drawCards);
     };
 
     return (
         <>
-            {cards.length === 0 ? (
-                <div className="container">
-                    <button type={'button'} onClick={dealCards}>Deal cards</button>
-                </div>
-            ) : (
-                <div className="container">
-                    <button type={'button'} onClick={dealCards}>Deal cards again</button>
-                    <div className="playingCards faceImages">
-                        {cards.map((card, index) => (
-                            <PlayingCard key={index} rank={card.rank} suit={card.suit}/>
-                        ))}
+            {
+                cards.length === 0 ? (
+                    <div className="container">
+                        <button type={'button'} onClick={dealCards}>Deal cards</button>
                     </div>
-                </div>
-            )
+                ) : (
+                    <div className="container">
+                        <button type={'button'} onClick={dealCards}>Deal cards again</button>
+                        <div className="playingCards faceImages">
+                            {cards.map((card, index) => (
+                                <PlayingCard key={index} rank={card.rank} suit={card.suit}/>
+                            ))}
+                        </div>
+                        <h4>
+                            {hand.getOutcome()}
+                        </h4>
+                    </div>
+                )
             }
         </>
     );
